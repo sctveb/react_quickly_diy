1e96d545f9d4f58f5b942e9a23b9650ea7c43df8
sctveb <sctveb@gmail.com>
2019-08-17 20:43:00 +0900

    2019-08-17-01 ch13 added

---
+++ ch13/router/jsx/about.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+
+module.exports = function About() {
+    return (
+        <div>
+            <a href="http://Node.University" target="_blank">Node.Univ</a>
+            is something blur blur...
+        </div>
+    )
+}
